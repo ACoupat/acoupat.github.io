dd4facf1d53047dda1028c063862fb101c464eb2
Antoine Coupat <15833106+ACoupat@users.noreply.github.com>
2022-08-08 14:53:59 +0100

    Standalone/tests (#13)

---
+++ 1d1j/service-worker.js
@@ -0,0 +1,20 @@
+var cacheName = 'jokes-pwa';
+var filesToCache = [
+    '/1d1j/',
+    '/1d1j/index.html',
+    '/1d1j/css/style.css',
+    '/1d1j/js/index.js'];
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+    e.respondWith(caches.match(e.request).then(function (response) {
+        return response || fetch(e.request);
+    })
+    );
+});
