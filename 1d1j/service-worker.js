d77f59d798803df9ff91f9555404382ede7180ef
acoupat <acoupat@github.com>
2022-08-21 16:08:33 +0100

    Add images to cache with Service Worker

--- 1d1j/service-worker.js
+++ 1d1j/service-worker.js
@@ -3,7 +3,9 @@ var filesToCache = [
     '/1d1j/',
     '/1d1j/index.html',
     '/1d1j/css/style.css',
-    '/1d1j/js/index.js'];
+    '/1d1j/js/index.js',
+    '/1d1j/images/*'
+];
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
